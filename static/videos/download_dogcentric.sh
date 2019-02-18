34f94d15a0912c1b6767a0c1a4032d2cf16b07aa
Tuan Anh Nguyen <39395178+mental689@users.noreply.github.com>
2019-02-18 17:40:57 +0900

    Added a comment

--- static/videos/download_dogcentric.sh
+++ static/videos/download_dogcentric.sh
@@ -3,6 +3,10 @@
 wget --recursive --no-parent -R "index.html*" ftp://robotics-ftp.ait.kyushu-u.ac.jp/dogcentric/
 
 PARENT=`pwd`
+## Under dogcentric directory, there are 4 sub-directories corresponding to 4 subjects (dogs).
+## The sub-directories seem to be named by dog names, and the segmented videos are archived in tar balls.
+## The following code do the following:
+## Go to each dog name directory and then extract segmented videos from tar balls.
 for name in 'Hime' 'Ku' 'Ringo' 'Ryu' ; do
     cd ${PARENT}/robotics-ftp.ait.kyushu-u.ac.jp/dogcentric/${name}/
     for i in `find ./*.tar.gz`; do
